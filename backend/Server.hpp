0a782bd1dd98050cf43f9bf223c0552592b36fc0
Krystian Wolniak <57046666+Orzyj@users.noreply.github.com>
2026-03-17 00:04:10 +0100

    Update

--- backend/Server.hpp
+++ backend/Server.hpp
@@ -1,12 +1,20 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
 #include <cstdlib>
 #include <iostream>
-
+#include <map>
+#include <string>
+#include <vector>
 #include <httplib.h>
+
 #include <nlohmann/json.hpp>
 
 #include <mongocxx/client.hpp>
 #include <mongocxx/instance.hpp>
 #include <mongocxx/uri.hpp>
+
+#include <bsoncxx/json.hpp>
 
 using json = nlohmann::json;
 
@@ -18,10 +26,28 @@ private:
     mongocxx::client m_client;
     httplib::Server m_svr;
 
+    // key , values
+    std::map<std::string, std::vector<std::string>> FILTER_KEYS {
+        {"classification",{"genre", "subgenre","theme", "difficulty"}},
+        {"technical_stats",{"engine", "average_playtime_hours","theme"}},
+        {"title",{}},
+        {"developer", {}},
+        {"release_year",{}}
+    };
+
+    std::vector<std::string> FILTER_KEYS_USER = {
+        "name", "email", "password"
+    };
+
     void setup_status_routes();
+    void setup_games_routes();
+    void setup_user_routes();
+    void seed_test_data();
 
 public:
     Server();
     void start();
 
 };
+
+#endif // SERVER.HPP
